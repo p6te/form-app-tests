a7adae8f951c8d50b294484ad8f90575e758d8f6
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-06-24 12:10:54 +0200

    add test to check initial inputs value

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-
-
 function App() {
   return (
-    <div className="App">
+    <div className="container my-5">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input type="text" id="email" className="form-control" name="email" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="form-control"
+            name="password"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="confirm-password" className="form-label">
+            Confirm password
+          </label>
+          <input
+            type="password"
+            id="confirm-password"
+            className="form-control"
+            name="confirm-password"
+          />
+        </div>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
+test("inputs should be initially empty", () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole("textbox");
+  const passwordInputElement = screen.getByLabelText("Password");
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+
+  expect(emailInputElement.value).toBe("");
+  expect(passwordInputElement.value).toBe("");
+  expect(confirmPasswordInputElement.value).toBe("");
+});
